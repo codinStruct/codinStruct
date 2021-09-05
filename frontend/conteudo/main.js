1215f0aceccd1e0c81c4f317666f9f6c61559c2f
DaviAMSilva <daviamsilva@hotmail.com>
2021-09-04 21:06:22 -0300

    Mudado a estrutura do projeto e alterações de estilo.
    Mudanças:
    - As pastas backend e frontend foram adicionadas
    - O arquivo exemplo.html foi movido para frontend/conteudo/index.html para que este seja o caminho para acessar os conteudos
    - A tag <main> foi adicionada para ser o pai do conteudo
    - A tag do conteudo foi mudada para <article>
    - O width do conteudo foi reduzido e centralizado
    - O padding foi aumentado um pouco

---
+++ frontend/conteudo/main.js
@@ -0,0 +1,45 @@
+// Show/hide sidebar items
+$(".categoria").on("click", function () {
+  // Change opened state
+  $(this).toggleClass("aberta");
+
+  // Rotating arrow
+  $(this).find("i.fas").toggleClass("fa-rotate-180");
+
+  // Show/hide children
+  $(this).parent().find("li").toggle("fast");
+});
+
+// Loading content, placing it and highlighting all 'pre code' with source code
+// TODO: In the future this loading will be done dynamically depending on the page visited
+$("#content").load(
+  "/Projeto-Content-Converted/C/Basico/primeiro_programa/primeiro_programa.html",
+  () =>
+    $("#content pre code").each((i, el) => {
+      hljs.highlightElement(el);
+      hljs.lineNumbersBlock(el, { singleLine: true });
+    })
+);
+
+// Swipes to open/close sidebar
+let barraLateral = $("#barra-lateral");
+
+$(document).on("swiped", function (e) {
+  switch (e.detail.dir) {
+    case "left":
+      // Sidebar swiped
+      if ($.contains(barraLateral[0], e.target)) {
+        barraLateral.removeClass("slide-in-left-mobile");
+        barraLateral.addClass("slide-out-left-mobile");
+      }
+      break;
+
+    case "right":
+      // Ignore swipes over horizontally scrollable elements
+      if (e.target.scrollWidth > e.target.clientWidth) return;
+
+      barraLateral.removeClass("slide-out-left-mobile");
+      barraLateral.addClass("slide-in-left-mobile");
+      break;
+  }
+});
