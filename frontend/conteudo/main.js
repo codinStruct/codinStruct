8c96ea0182da2374df224e4b651ad101b199e129
Luiz Fernando F. G. Valle <60049320+luizffgv@users.noreply.github.com>
2021-09-05 05:15:42 -0300

    show/hide da sidebar agora usam jQuery UI

--- frontend/conteudo/main.js
+++ frontend/conteudo/main.js
@@ -1,5 +1,5 @@
 // Show/hide sidebar items
-$(".categoria").on("click", function () {
+$(".categoria").click(function () {
   $(this)
     // Change opened state
     .toggleClass("aberta")
@@ -31,16 +31,14 @@ let barraLateral = $("#barra-lateral");
 $(document).on("swiped", function (e) {
   switch (e.detail.dir) {
     case "left":
-      barraLateral.removeClass("slide-in-left-mobile");
-      barraLateral.addClass("slide-out-left-mobile");
+      // .stop so animations don't queue
+      barraLateral.stop(false, true).hide("slide");
       break;
 
     case "right":
       // Ignore swipes over horizontally scrollable elements
       if (e.target.scrollWidth > e.target.clientWidth) return;
-
-      barraLateral.removeClass("slide-out-left-mobile");
-      barraLateral.addClass("slide-in-left-mobile");
+      barraLateral.stop(false, true).show("slide");
       break;
   }
 });
