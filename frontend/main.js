fe13464cff50cfdd544a4b549db05fe8c107c974
DaviAMSilva <daviamsilva@hotmail.com>
2021-09-05 15:45:41 -0300

    Adicionado a nova página inicial do site utilizado a biblioteca p5.js para criar um background animado importado por meio de um <iframe>
    Outras mudanças:
    - Agora todos os arquivos .css e .css.map são ignorados com a exceção de styles/css/animations.css
    - Adicionada uma imagem PLACEHOLDER representando o logo que será usado
    - Os scripts de sass agora também contém os arquivos scss locais

---
+++ frontend/main.js
@@ -0,0 +1,4 @@
+// Clicking the button scrolls to the next page
+$("#next-page-button").on("click", function () {
+  $("#second-page")[0].scrollIntoView();
+});
--- sass_watch.sh
+++ sass_watch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sass --watch styles/sass/:styles/css/
+sass --watch styles/sass/:styles/css/ frontend/style.scss:frontend/style.css
