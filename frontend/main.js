31df5ad8bf2aff4f8814bcddbd05a4d64b752afc
Luiz Fernando F. G. Valle <60049320+luizffgv@users.noreply.github.com>
2021-09-05 22:49:27 -0300

    Remove animações e 3D no #introduction
    - Fundos transparentes levemente mais opacos
    - #next-page-button agora usa o mixin box

--- frontend/main.js
+++ frontend/main.js
@@ -2,22 +2,3 @@
 $("#next-page-button").on("click", function () {
   $("#second-page")[0].scrollIntoView();
 });
-
-let html = $("html");
-let introduction = $("#introduction");
-
-// Vertical scrolling rotates the introduction
-document.addEventListener("scroll", function () {
-  // TODO: Optimize this function. It runs even when element is outside the viewport
-
-  // Ranges from 0 to 1 when the first page is off the screen
-  let progress = Math.min(1, html.scrollTop() / window.innerHeight);
-  window.requestAnimationFrame(function () {
-    introduction.css(
-      "transform",
-      `perspective(max(100vw, 100vh)) rotateY(${180 * progress}deg)`
-    );
-
-    introduction.find("> *").css("transform", `translateZ(${5 * progress}em)`);
-  });
-});
