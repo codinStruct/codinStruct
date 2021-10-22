48f8d52140cec480ae9d46c749847a8422ed242a
DaviAMSilva <daviamsilva@hotmail.com>
2021-10-22 17:59:53 -0300

    Usando nova versão do conversor de markdown

--- content_md2html.sh
+++ content_md2html.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export PIPENV_PIPFILE=Projeto-MDtoHTML/Pipfile
-python3 -m pipenv run python3 Projeto-MDtoHTML/app.py codinStruct-content/Content codinStruct-content-Converted
+python3 Projeto-MDtoHTML/app.py codinStruct-content/Content codinStruct-content-Converted
