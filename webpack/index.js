fc6133122190077c0efdc10640a7898607de339b
DaviAMSilva <daviamsilva@hotmail.com>
2021-11-27 19:25:09 -0300

    Atualizado as depedências do npm Substituido a package p5js por p5

--- webpack/index.js
+++ webpack/index.js
@@ -1,5 +1,5 @@
 window.$ = require('jquery');
-window.p5 = require('p5js/p5.js/p5.min.js');
+window.p5 = require('p5');
 
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import '@fortawesome/fontawesome-free/css/solid.min.css';
