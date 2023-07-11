19a07f42c8ff3f596762ba557465e03579260f9c
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2023-07-11 17:31:27 -0300

    Usar venv pro md2html

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,19 @@
-FROM ubuntu
+FROM node:20
+
+EXPOSE 3000/tcp
 
 WORKDIR /usr/src/codinStruct
 RUN apt-get update
-RUN apt-get install -y nodejs
-RUN apt-get install -y npm
 RUN apt-get install -y python3
 RUN apt-get install -y python3-pip
 
 COPY package*.json ./
 RUN npm install
 
-COPY md2html/requirements.txt md2html/
-RUN pip3 install -r md2html/requirements.txt
-
 COPY . .
 
 # Saves the built files to the image so they don't have to be built again
-# every time Heroku stops the app because it was idle and has to start it again 
+# every time Heroku stops the app because it was idle and has to start it again
 RUN npm run build:prod
 
 CMD ["npm","start"]
---
+++ md2html-run.ts
@@ -0,0 +1,28 @@
+import { spawnSync } from "child_process";
+
+/**
+ * Shows an error and stops execution.
+ *
+ * @param what - Error to show.
+ */
+function error(what: string): never {
+  console.error(`md2html venv activation error: ${what}`);
+  process.exit(1);
+}
+
+let command: string | undefined;
+switch (process.platform) {
+  case "linux":
+    command =
+      "source ./md2html/.venv/bin/activate && python3 md2html/app.py codinStruct-content/Content frontend/content";
+    break;
+
+  case "win32":
+    command =
+      ".\\md2html\\.venv\\Scripts\\activate && python3 md2html\\app.py codinStruct-content\\Content frontend\\content";
+    break;
+}
+if (command == null) error("Unsupported platform");
+
+const spawned = spawnSync(command, { shell: true });
+if (spawned.error) Error(spawned.error.message);
